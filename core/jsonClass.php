736a2962286edf19f4b92e06964796114d2376d2
SIV <siv@sibes.omsk.ru>
2015-12-08 17:24:16 +0600

    add method 2 jsonClass

--- core/jsonClass.php
+++ core/jsonClass.php
@@ -6,8 +6,34 @@
  */
 class jsonClass 
 {
-    public function setJsonString()
+    public $jsonString;
+    public $jsonDecoded;
+    public $coords = [];
+            
+    function __construct($json)
     {
-        $jsonString = "";
+      $this->jsonString = $json;
+    }
+    
+    public function getJson()
+    {
+      return $this->jsonString;
+    }
+    
+    public  static function Olol()
+    {
+      return "ololo";
+    }
+    
+    public static function parsingJson()
+    {
+      $this->jsonDecoded = json_decode($this->jsonString);
+      return $this->jsonDecoded->response->GeoObjectCollection->featureMember[0]->GeoObject->Point->pos;
+    }
+    
+    public function cuttingCoord()
+    {
+      $coords = explode(" ", self::parsingJson());
+      return $coords;         
     }
 }
--- index.php
+++ index.php
@@ -4,6 +4,7 @@ define('path', __DIR__);
 
 include(path . '/core/configClass.php');
 include(path . '/core/dbClass.php');
+include(path . '/core/jsonClass.php');
 include_once 'header.php';
 include_once 'regform.php';
 
@@ -14,4 +15,16 @@ while ($r = $q->fetch())
 {
   echo $r['id'] . " | " . $r['username'] . " | " . $r['email'] . "<br>";
 }
+
+$a = '{"response":{"GeoObjectCollection":{"metaDataProperty":{"GeocoderResponseMetaData":{"request":"Омск, 33 северная 122","found":"1","results":"10"}},"featureMember":[{"GeoObject":{"metaDataProperty":{"GeocoderMetaData":{"kind":"house","text":"Россия, Омск, Амурский поселок, 33-я Северная улица, 122","precision":"exact","AddressDetails":{"Country":{"AddressLine":"Омск, Амурский поселок, 33-я Северная улица, 122","CountryNameCode":"RU","CountryName":"Россия","AdministrativeArea":{"AdministrativeAreaName":"Омская область","SubAdministrativeArea":{"SubAdministrativeAreaName":"городской округ Омск","Locality":{"LocalityName":"Омск","DependentLocality":{"DependentLocalityName":"Амурский поселок","Thoroughfare":{"ThoroughfareName":"33-я Северная улица","Premise":{"PremiseNumber":"122"}}}}}}}}}},"description":"Амурский поселок, Омск, Россия","name":"33-я Северная улица, 122","boundedBy":{"Envelope":{"lowerCorner":"73.398207 55.024714","upperCorner":"73.414664 55.034167"}},"Point":{"pos":"73.406435 55.029441"}}}]}}}';
+
+$testjson = new jsonClass($a);
+
+
+//echo var_dump($testjson);
+echo "<br><br>";
+echo $testjson::Olol();
+echo "<br><br>";
+echo var_dump($testjson::parsingJson());
+echo var_dump($testjson->cuttingCoord());
 include_once 'footer.php';
