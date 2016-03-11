437a409f9e996a8bfadd85fed260a25514841f3f
SIV <siv@sibes.omsk.ru>
2016-03-11 13:22:53 +0600

    add userregform

--- index.php
+++ index.php
@@ -6,7 +6,7 @@ include_once path . '/core/dbClass.php';
 include_once 'address.php';
 include_once 'header.php';
 
-include 'regform.php';
+include 'userRegForm.php';
 
 
 
---
+++ userRegForm.php
@@ -0,0 +1,26 @@
+<div class="row">
+  <div class="col s12 m4 l2">&nbsp;</div>
+  <div class="col s12 m6 l8">
+      <form action="action" method="">
+          <div class="row">
+            <div class="input-field col s12">
+                <input placeholder="Ваше имя" name="username" id="username" type="text" class="validate">
+                <label for="username">Имя пользователя</label>
+            </div>
+            <div class="input-field col s12">
+              <input name="email" id="email" type="email" class="validate">
+              <label for="email">электронная почта</label>
+            </div>
+            <div class="input-field col s12">
+              <input name="password" id="password" type="password" class="validate">
+              <label for="email">Ваш пароль</label>
+            </div>
+          </div>
+          <button class="btn waves-effect waves-light btn_clr2" type="submit" name="reg">Зарегистрироваться
+        <i class="fa fa-share"></i>
+      </button>
+      </form>
+  </div>
+  <div class="col s12 m4 l2">&nbsp;</div>
+</div>
+
