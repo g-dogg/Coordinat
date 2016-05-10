6f7116c67773ee124cb9432f9beb6bfeb04533f5
g-dogg <g_dogg@xakep.ru>
2016-05-10 16:58:00 +0600

    added router,loader classes

--- core/loaderClass.php
+++ core/loaderClass.php
@@ -2,17 +2,5 @@
 
 class loaderClass
 {
-	private $requestUrl;
-	private $componentsArray = [];
-
-	function __constructor()
-	{
-		$this->requestUrl = $_SERVER['REQUEST_URI'];
-	}
-
-	public function getUrlComponents()
-	{
-		$this->componentsArray = explode("/", $this->requestUrl);
-	}
 
 }
