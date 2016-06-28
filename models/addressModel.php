9994e17f52aef794bbb723f0f92020ead1e2d75a
g-dogg <g_dogg@xakep.ru>
2016-06-28 22:15:12 +0600

    modify addressModel

--- models/addressModel.php
+++ models/addressModel.php
@@ -3,4 +3,17 @@
 class addressModel extends Model
 {
 	//TODO: свойства по полям таблицы, методы получения и установки . метод проверки и запроса координат. добавление новых записей
+    private $fullAddress = [];
+    
+    public function setNewFullAddress()
+    {
+        $query = "SELECT * FROM towns, address, coordinats
+                  WHERE towns.id = address.townid AND 
+                  address.coordinat.id = coordinats.id AND 
+                  coordinats.status = 0 LIMIT 1";
+        $handler = $this->db->query($query, PDO::FETCH_ASSOC);
+        $this->fullAddress = $handler->fetch();
+        return $this;
+    }
+    
 }
