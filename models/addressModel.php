caa5e0c66eb2c8798004ed36ba19579b3283a564
g-dogg <g_dogg@xakep.ru>
2016-07-04 15:22:38 +0600

    маленький эдит addressModel

--- models/addressModel.php
+++ models/addressModel.php
@@ -5,30 +5,31 @@ class addressModel extends Model
 	//TODO: свойства по полям таблицы, методы получения и установки . метод проверки и запроса координат. добавление новых записей
     private $fullAddress = [];
     private $serviceUrl = 'https://geocode-maps.yandex.ru/1.x/?format=json&geocode=';
-    
-    
+
+
     public function setNewFullAddress()
     {
         $query = "SELECT * FROM towns, address, coordinats
-                  WHERE towns.id = address.townid AND 
-                  address.coordinat.id = coordinats.id AND 
+                  WHERE towns.id = address.townid AND
+                  address.coordinat.id = coordinats.id AND
                   coordinats.status = 0 LIMIT 1";
         $handler = $this->db->query($query, PDO::FETCH_ASSOC);
         $this->fullAddress = $handler->fetch();
     }
-    
+
     public function getNewFullAddress()
     {
         return $this->fullAddress;
     }
-    
+
     public function getAddressToRequest()
     {
-        $addrForReq = $this->fullAddress['town'] . $this->fullAddress['street'] . " "; //TODO: допилить склейку адреса для запроса
+        $addrForReq = $this->fullAddress['town'] . $this->fullAddress['street'] . ", " . $this->fullAddress['build'] . ", " . $this->fullAddress['housing']; //может и не housing, а corp
+        return $addrForReq;
     }
-    
+
     public function requestNewCoords()
     {
-        $curl = new Curl($this->serviceUrl, $this->fullAddress);
+        $curl = new Curl($this->serviceUrl, $this->getAddressToRequest());
     }
 }
